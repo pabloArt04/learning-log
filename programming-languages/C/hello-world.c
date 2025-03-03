940aee97b28e1b251c4ac501f8fc62d2d4415a14
Pablo Arturo <pabloart04.code@gmail.com>
2025-03-02 18:41:52 -0600

    (programming-languages) hello world in C

---
+++ programming-languages/C/hello-world.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Include the standard input/output library
+
+// The main function is the entry point of the program
+int main() {
+    puts("Hello, World!"); // Print "Hello, World!" to the console
+    return 0; // return a status code of 0 to indicate success to the operating system
+}
